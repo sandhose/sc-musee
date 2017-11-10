0db6437028a98e4ac08d7df90998b747ead80421
Quentin Gliech <quentingliech@gmail.com>
2017-11-10 22:18:08 +0100

    controleur: Sleep at the beginning of the loop

--- controleur.c
+++ controleur.c
@@ -21,13 +21,14 @@ int main(int argc, char *argv[]) {
   // Waiting until the first semaphore goes to zero
   SEMOPS(semid, {SEM_CLOSED, 0, 0});
   INFOF("Le musée est ouvert ! (capacité: %d, file: %d)", m->capacite, m->file);
-  TRY_SYS(semctl(semid, SEM_SLEEP, SETVAL, 1), "semctl");
+  TRY_SYS(semctl(semid, SEM_SLEEP, SETVAL, 0), "semctl");
   DEBUG("Marque en attente de travail");
   TRY_SYS(semctl(semid, SEM_CAPACITY, SETVAL, m->capacite), "semctl");
   DEBUG("Enregistre la capacité initiale");
 
   short last_inside = 0, inside;
   while (get_sem_val(semid, SEM_CLOSED) == 0 || last_inside > 0) {
+    SEMOPS(semid, {SEM_SLEEP, 0, 0}, {SEM_SLEEP, 1, 0});
     inside = (short)get_sem_val(semid, SEM_INSIDE);
     DEBUGF("Tick. %d personnes dans le musée.", inside);
     if (last_inside > inside) {
@@ -35,9 +36,9 @@ int main(int argc, char *argv[]) {
       SEMOPS(semid, {SEM_CAPACITY, last_inside - inside, 0});
     }
     last_inside = inside;
-
-    SEMOPS(semid, {SEM_SLEEP, 0, 0}, {SEM_SLEEP, 1, 0});
   }
+
+  INFO("Le musée est fermé, et il n'y a plus de visiteurs.");
 
   return 0;
 }
