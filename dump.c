2ef0cc84bf4ede410f11821ba5a6365f0a9f589d
Quentin Gliech <quentingliech@gmail.com>
2017-11-10 22:17:28 +0100

    Simple dump program

---
+++ dump.c
@@ -0,0 +1,25 @@
+// Print everything regardless of the environment variable
+#define LOG_LEVEL 5
+
+#include "musee.h"
+
+static void usage(const char *prgname) {
+  fprintf(stderr, "usage: %s\n", prgname);
+}
+
+int main(int argc, char *argv[]) {
+  TRY_OR_USAGE(argc == 1, "Ne prend aucun argument.");
+  struct musee *m = get_musee(get_shm());
+  int semid = get_sem();
+
+  int waiting, inside, closed;
+  TRY_SYS(waiting = semctl(semid, SEM_CAPACITY, GETNCNT), "semctl");
+  TRY_SYS(inside = semctl(semid, SEM_INSIDE, GETVAL), "semctl");
+  TRY_SYS(closed = semctl(semid, SEM_CLOSED, GETVAL), "semctl");
+
+  INFOF("     Capacité: %d", m->capacite);
+  INFOF("         File: %d", m->file);
+  INFOF("         État: %s", closed ? "fermé" : "ouvert");
+  INFOF("   En attente: %d", waiting);
+  INFOF("À l'intérieur: %d", inside);
+}
